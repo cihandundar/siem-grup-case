220992321c29f4a6bdab1748411eddbc9aba70a7
cihandundar <94462152+cihandundar@users.noreply.github.com>
2024-07-20 00:07:14 +0300

    created slice to add book

--- src/features/book/bookSlice.js
+++ src/features/book/bookSlice.js
@@ -1,5 +1,6 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
+import { toast } from "react-toastify";
 
 const initialState = {
   data: [],
@@ -15,6 +16,20 @@ export const fetchBook = createAsyncThunk("fetchBook", async () => {
   return response.data;
 });
 
+export const addNewBook = createAsyncThunk("users/addNewBook", async (body) => {
+  try {
+    const response = await axios.post(
+      `https://64542599c18adbbdfeb058b1.mockapi.io/posts`,
+      body
+    );
+    toast.success("Book added successfully");
+    return response.data;
+  } catch (error) {
+    console.error("Error adding new book: ", error.message);
+    throw error;
+  }
+});
+
 export const bookSlice = createSlice({
   name: `books`,
   initialState,
@@ -28,6 +43,17 @@ export const bookSlice = createSlice({
       state.isLoading = false;
     });
     builder.addCase(fetchBook.rejected, (state, action) => {
+      state.error = action.error.message;
+      state.isLoading = false;
+    });
+    builder.addCase(addNewBook.pending, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(addNewBook.fulfilled, (state, action) => {
+      state.data = [...state.data, action.payload];
+      state.isLoading = false;
+    });
+    builder.addCase(addNewBook.rejected, (state, action) => {
       state.error = action.error.message;
       state.isLoading = false;
     });
