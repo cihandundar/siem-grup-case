0a3a04ece0cb5043d7d2e488d401846fd567daa7
cihandundar <94462152+cihandundar@users.noreply.github.com>
2024-07-19 23:28:47 +0300

    axios loaded

--- src/pages/Book/index.js
+++ src/pages/Book/index.js
@@ -1,7 +1,44 @@
-import React from "react";
-
+import { fetchBook } from "../../features/book/bookSlice";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import "../../index.css";
 const Book = () => {
-  return <div></div>;
+  const data = useSelector((state) => state.books.data);
+  const isLoading = useSelector((state) => state?.books?.isLoading);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchBook());
+  }, [dispatch]);
+
+  return (
+    <section>
+      <div className="container">
+        <h1>Book List</h1>
+
+        {isLoading ? (
+          <div className="loading">
+            <span className="loader"></span>
+          </div>
+        ) : (
+          <div className="wrapper">
+            {data.map((book) => (
+              <div key={book.id} className="cart">
+                <div className="cart-title">
+                  <h2>{book.title}</h2>
+                </div>
+                <div className="cart-image">
+                  <img src={book.avatar} alt="" />
+                </div>
+                <div className="cart-name">
+                  <p>{book.name}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
+  );
 };
 
 export default Book;
--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,100 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
+import { login } from "../../features/auth/authSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-toastify";
+import "../../index.css";
 const SignIn = () => {
-  return <div></div>;
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const isLoading = useSelector((state) => state?.books?.isLoading);
+  const { state } = useLocation();
+  const previousPath = state?.from ? state.from : "/";
+  const { user, isError, isSuccess, message } = useSelector(
+    (state) => state.auth
+  );
+
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = formData;
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const userData = {
+      email,
+      password,
+    };
+    dispatch(login(userData));
+  };
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message);
+    }
+
+    if (isSuccess || user) {
+      navigate(previousPath);
+    }
+  }, [user, isError, isSuccess, message, navigate, dispatch]);
+
+  return (
+    <section className="register">
+      {isLoading ? (
+        <div className="loading">
+          <span className="loader"></span>
+        </div>
+      ) : (
+        <div className="register-container">
+          <div className="right">
+            <div>
+              <h1>Sign In</h1>
+            </div>
+            <div>
+              <form onSubmit={onSubmit} className="form">
+                <div className="form-group">
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="email"
+                    name="email"
+                    value={email}
+                    placeholder="Email"
+                    onChange={onChange}
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="password"
+                    name="password"
+                    value={password}
+                    placeholder="Password"
+                    onChange={onChange}
+                  />
+                </div>
+
+                <div className="form-group">
+                  <button type="submit">Sign in</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      )}
+    </section>
+  );
 };
 
 export default SignIn;
