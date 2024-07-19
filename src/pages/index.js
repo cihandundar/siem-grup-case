f711a65393d2c2ea39596de79288d0e67b411b44
cihandundar <94462152+cihandundar@users.noreply.github.com>
2024-07-19 22:20:39 +0300

    pages folders created and imported

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import Home from "./Home";
+import SignIn from "./SignIn";
+import SignUp from "./SignUp";
+import Book from "./Book";
+import BookDetails from "./BookDetails";
+import AddBook from "./AddBook";
+import EditBook from "./EditBook";
+export { SignIn, SignUp, Home, Book, BookDetails, AddBook, EditBook };
