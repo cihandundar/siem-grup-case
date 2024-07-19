c19f4975a2640fc5b2f0f7f36e201119857b0749
cihandundar <94462152+cihandundar@users.noreply.github.com>
2024-07-20 00:07:28 +0300

    edited book addition page

--- src/pages/AddBook/index.js
+++ src/pages/AddBook/index.js
@@ -1,7 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { addNewBook } from "../../features/book/bookSlice";
 
 const AddBook = () => {
-  return <div></div>;
+  const dispatch = useDispatch();
+  const [name, setName] = useState("");
+  const [title, setTitle] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const body = { name, title };
+    dispatch(addNewBook(body));
+    setTimeout(() => {
+      navigate("/book");
+    }, 2000);
+  };
+
+  return (
+    <section className="section">
+      <div className="section-container">
+        <div className="section-container-title">
+          <h2>Add Post</h2>
+        </div>
+        <form onSubmit={handleSubmit}>
+          <div className="container-item">
+            <label>Author</label>
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
+          </div>
+          <div className="container-item">
+            <label>Name</label>
+            <textarea
+              required
+              type="text"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </div>
+          <button>Add Blog</button>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default AddBook;
