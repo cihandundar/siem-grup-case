b3e48715ac75ff6b1021f63adb4e84849bef96d9
cihandundar <94462152+cihandundar@users.noreply.github.com>
2024-07-19 23:31:25 +0300

    private router structure created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,79 @@
+import { Navbar } from "./components";
+import { SignIn, SignUp } from "./pages";
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import { ToastContainer } from "react-toastify";
+// import "react-toastify/dist/ReactToastify.css";
+import PrivateRoute from "./components/PrivateRoute";
+import Error from "./pages/Error";
+import { AddBook, Book, BookDetails, EditBook, Home } from "./pages";
 function App() {
+  const route = [
+    {
+      path: "/signin",
+      element: <SignIn />,
+    },
+    {
+      path: "/signup",
+      element: <SignUp />,
+    },
+    {
+      path: "*",
+      element: <Error />,
+    },
+    {
+      path: "/book",
+      element: <Book />,
+    },
+    {
+      element: <PrivateRoute />,
+      routes: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+
+        {
+          path: "/book/:id",
+          element: <BookDetails />,
+        },
+        {
+          path: "/editbook/:postId",
+          element: <EditBook />,
+        },
+        {
+          path: "/addbook",
+          element: <AddBook />,
+        },
+      ],
+    },
+  ];
+
   return (
-    <div className="App">
-      <h1>Deneme 1</h1>
-    </div>
+    <BrowserRouter>
+      {/* <ToastContainer position="top-right" /> */}
+      <Navbar />
+      <Routes>
+        {route.map((route, index) => {
+          if (route.routes) {
+            return (
+              <Route element={route.element} key={index}>
+                {route.routes.map((subRoute, subIndex) => (
+                  <Route
+                    element={subRoute.element}
+                    path={subRoute.path}
+                    key={subIndex}
+                  />
+                ))}
+              </Route>
+            );
+          }
+          return (
+            <Route key={index} path={route.path} element={route.element} />
+          );
+        })}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
