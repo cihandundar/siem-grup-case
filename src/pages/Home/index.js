da57d16ab4cf4b06e0d3bca5c979dea78180a576
cihandundar <94462152+cihandundar@users.noreply.github.com>
2024-07-20 13:22:16 +0300

    home page fix

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,33 @@
 import React from "react";
-
+import hero from "../../assets/hero.jpg";
+import { Link } from "react-router-dom";
 const Home = () => {
-  return <div></div>;
+  return (
+    <section>
+      <div className="container">
+        <Link
+          style={{
+            position: "absolute",
+            padding: "10px 30px",
+            backgroundColor: "red",
+            zIndex: "1",
+            left: "50%",
+            top: "50%",
+            transform: "translateX(-50%)",
+            color: "#fff",
+          }}
+          to="/book"
+        >
+          Go Book List
+        </Link>
+        <img
+          style={{ width: "100%", position: "relative" }}
+          src={hero}
+          alt=""
+        />
+      </div>
+    </section>
+  );
 };
 
 export default Home;
