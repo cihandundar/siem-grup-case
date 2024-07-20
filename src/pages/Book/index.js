708699674c906a945fa4ebd40278b46fc8ab06f3
cihandundar <94462152+cihandundar@users.noreply.github.com>
2024-07-20 13:23:37 +0300

    added delete and drill down

--- src/pages/Book/index.js
+++ src/pages/Book/index.js
@@ -1,7 +1,8 @@
-import { fetchBook } from "../../features/book/bookSlice";
+import { fetchBook, handleDelete } from "../../features/book/bookSlice";
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import "../../index.css";
+import { Link } from "react-router-dom";
 const Book = () => {
   const data = useSelector((state) => state.books.data);
   const isLoading = useSelector((state) => state?.books?.isLoading);
@@ -9,6 +10,10 @@ const Book = () => {
   useEffect(() => {
     dispatch(fetchBook());
   }, [dispatch]);
+
+  const handleDeleteFunction = (id) => {
+    dispatch(handleDelete(id));
+  };
 
   return (
     <section>
@@ -22,14 +27,27 @@ const Book = () => {
           <div className="wrapper">
             {data.map((book) => (
               <div key={book.id} className="cart">
-                <div className="cart-image">
-                  <img src={book.imageURL || book.avatar} alt={book.title} />
-                </div>
+                <Link to={`/book/${book?.id}`}>
+                  <div className="cart-image">
+                    <img src={book.imageURL || book.avatar} alt={book.title} />
+                  </div>
+                </Link>
                 <div className="cart-title">
                   <h3>{book.title}</h3>
                 </div>
                 <div className="cart-name">
                   <p>{book.name}</p>
+                </div>
+                <div className="cart-btn">
+                  <button
+                    className="delete"
+                    onClick={() => handleDeleteFunction(book?.id)}
+                  >
+                    Delete
+                  </button>
+                  <Link to={`/editbook/${book?.id}`}>
+                    <button className="edit">Edit</button>
+                  </Link>
                 </div>
               </div>
             ))}
