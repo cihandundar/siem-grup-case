4e20f30962dac4376d46aa603cf3bfaa926299c4
cihandundar <94462152+cihandundar@users.noreply.github.com>
2024-07-20 13:23:19 +0300

    book detail page added

--- src/pages/BookDetails/index.js
+++ src/pages/BookDetails/index.js
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { fetchBookDetails } from "features/book/bookSlice";
 
-const BookDetails = () => {
-  return <div></div>;
+const PostDetails = () => {
+  const details = useSelector((state) => state?.books?.details);
+  const dispatch = useDispatch();
+  const isLoading = useSelector((state) => state?.users?.isLoading);
+  const { id } = useParams();
+  useEffect(() => {
+    dispatch(fetchBookDetails(id));
+  }, [dispatch, id]);
+
+  return (
+    <div className="details">
+      <div className="details-container">
+        {isLoading ? (
+          <div className="loading">
+            <span className="loader"></span>
+          </div>
+        ) : (
+          <div className="details">
+            <div className="details-cart">
+              <div className="details-text">
+                <img
+                  src={details.imageURL || details.avatar}
+                  alt={details.title}
+                />
+              </div>
+              <div className="details-text">
+                <h2>{details?.title}</h2>
+              </div>
+              <div className="details-text">
+                <h3>{details?.name}</h3>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
-export default BookDetails;
+export default PostDetails;
